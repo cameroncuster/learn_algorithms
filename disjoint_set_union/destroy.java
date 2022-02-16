26df29a4195cbdb6d24e8a934a52bac579296330
Cameron Custer <ccuster@banff.ptl.ucf.edu>
2022-02-15 19:26:29 -0500

    uses Buffered IO for speed

--- disjoint_set_union/destroy.java
+++ disjoint_set_union/destroy.java
@@ -22,71 +22,11 @@
 import java.io.*;
 import java.util.*;
 
-// disjoint set union object
-class DSU {
-	// parent array
-	int p[];
-	public long sumOfSquares;
-
-	// construct the DSU
-	public DSU(int n) {
-		p = new int[n];
-		Arrays.fill(p, -1);
-
-		// sum of 1^2 [0, n) = n
-		sumOfSquares = n;
-	}
-
-	// find the root
-	int find(int i) {
-		// we found the root
-		if (p[i] < 0)
-			return i;
-
-		// point all nodes on the path to the root (path compression)
-		return p[i] = find(p[i]);
-	}
-
-	// the size is stored in the parent array at the root (negated)
-	int size(int i) {
-		return -p[find(i)];
-	}
-
-	// union components i and j
-	void union(int i, int j) {
-		// same component?
-		if ((i = find(i)) == (j = find(j)))
-			return;
-
-		// make i bigger set
-		if (p[i] > p[j]) {
-			int temp = i;
-			i = j;
-			j = temp;
-		}
-
-		// remove old comp sizes squared
-		long a = size(i);
-		long b = size(j);
-		sumOfSquares -= a * a;
-		sumOfSquares -= b * b;
-
-		// merge j into i
-		p[i] += p[j];
-		p[j] = i;
-
-		// add new comp size squared
-		long c = size(i);
-		sumOfSquares += c * c;
-	}
-}
-
 // main program class
 class destroy {
-
 	// the main function
 	public static void main(String args[]) {
-		Scanner stdin = new Scanner(System.in);
+		FastScanner stdin = new FastScanner();
 
 		// capture meta data
 		int n = stdin.nextInt();
@@ -126,9 +66,98 @@ class destroy {
 			connectivity[i] = dsu.sumOfSquares;
 		}
 
+		// buffered output stream
+		PrintWriter out = new PrintWriter(System.out);
+
 		// output the answer to the queries
 		for (int i = 0; i <= k; i++)
-			System.out.println(connectivity[i]);
+			out.println(connectivity[i]);
+		out.close();
 	}
 
+	// disjoint set union object
+	static class DSU {
+		// parent array
+		int p[];
+		public long sumOfSquares;
+
+		// construct the DSU
+		public DSU(int n) {
+			p = new int[n];
+			Arrays.fill(p, -1);
+
+			// sum of 1^2 [0, n) = n
+			sumOfSquares = n;
+		}
+
+		// find the root
+		int find(int i) {
+			// we found the root
+			if (p[i] < 0)
+				return i;
+
+			// point all nodes on the path to the root (path compression)
+			return p[i] = find(p[i]);
+		}
+
+		// the size is stored in the parent array at the root (negated)
+		int size(int i) {
+			return -p[find(i)];
+		}
+
+		// union components i and j
+		void union(int i, int j) {
+			// same component?
+			if ((i = find(i)) == (j = find(j)))
+				return;
+
+			// make i bigger set
+			if (p[i] > p[j]) {
+				int temp = i;
+				i = j;
+				j = temp;
+			}
+
+			// remove old comp sizes squared
+			long a = size(i);
+			long b = size(j);
+			sumOfSquares -= a * a;
+			sumOfSquares -= b * b;
+
+			// merge j into i
+			p[i] += p[j];
+			p[j] = i;
+
+			// add new comp size squared
+			long c = size(i);
+			sumOfSquares += c * c;
+		}
+	}
+
+	// buffered IO for speed
+	static class FastScanner {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer("");
+
+		String next() {
+			while (!st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (Exception e) { };
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+	}
 }
